f58968859c72c2988493e2cb1d9e26c67d77ed42
Schyler Hosler <schylerhosler@gmail.com>
2016-10-23 21:00:19 -0400

    Ruby practice to better understand Queues vs Stacks -- all tests passing

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,22 @@
 require_relative 'stack'
+require 'pry'
 
 class Queue
+
+  attr_accessor :stack1, :stack2
+
+  def initialize
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+  end
+
+  def enqueue(data)
+    self.stack1.push(data)
+    self.stack2.unshift(data)
+  end
+
+  def dequeue
+    self.stack2.pop
+  end
 
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,29 @@
 class Stack
 
+  attr_reader :array
+
+  def initialize
+    @array = []
+  end
+
+  def pop
+    self.array.pop
+  end
+
+  def push(data)
+    self.array.push(data)
+  end
+
+  def unshift(data)
+    self.array.unshift(data)
+  end
+
+  def shift
+    self.array.shift
+  end
+
+  def size
+    return @array.size
+  end
+
 end
